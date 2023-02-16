5711c00daa68d0caef9053fe3a0eb283ca0ea507
ChadPerkins <web3.engineering@gmail.com>
2023-02-16 14:17:12 -0500

    Completed excercise 1.7

--- part1/Excerxises1.6-1.14/src/App.js
+++ part1/Excerxises1.6-1.14/src/App.js
@@ -6,18 +6,26 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-
+	const total = good + neutral + bad
+    const average = (good * 1 + bad * -1) / total
+    const positive = good * (100/total)
 	
 	return (
 		<div>
 			<Title text={"give feedback"} />
-			<Button handleClick={() => setGood (good + 1)} review={"good"} />
-			<Button handleClick={() => setNeutral(neutral + 1)} review={"neutral"} />
+			<Button handleClick={() => setGood(good + 1)} review={"good"} />
+			<Button
+				handleClick={() => setNeutral(neutral + 1)}
+				review={"neutral"}
+			/>
 			<Button handleClick={() => setBad(bad + 1)} review={"bad"} />
 			<Title text={"statistics"} />
 			<Review review={good} text={"good"} />
 			<Review review={neutral} text={"neutral"} />
 			<Review review={bad} text={"bad"} />
+			<Review review={total} text={"all"} />
+			<Average review={average} text={"average"} />
+			<Review review={positive} text={"positive"} />
 		</div>
 	);
 };
@@ -28,9 +36,17 @@ const Button = ({ handleClick, review }) => {
 	return <button onClick={handleClick}>{review}</button>;
 };
 
-const Title = ({text}) => {
-	return <h1>{text}</h1>
-}
-const Review = ({review, text}) => {
-	return <p>{text} {review}</p>
-}
+const Title = ({ text }) => {
+	return <h1>{text}</h1>;
+};
+const Review = ({ review, text }) => {
+	return (
+		<p>
+			{text} {review}
+		</p>
+	);
+};
+
+const Average = ({ review, text }) => {
+	return <p>{text} {review}</p>;
+};
