07072379ee10c58014f64a8223d0737e6e8c1480
ChadPerkins <web3.engineering@gmail.com>
2023-02-20 14:14:21 -0500

    Completed exercise 2.8

--- part2/exercises2.6-2.10/src/App.js
+++ part2/exercises2.6-2.10/src/App.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", phone: "123-456-7890" },
+	]);
 	const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
 	const addNewName = (event) => {
 		event.preventDefault();
-		
-    if(persons.some(e => e.name === newName)) {
-      alert(`${newName} is already added to the phonebook`)
-    } else {
-      setPersons(persons.concat({ name: newName}))
-    }
-    console.log(persons);
-		
+
+		if (persons.some((e) => e.name === newName)) {
+			alert(`${newName} is already added to the phonebook`);
+		} else {
+			setPersons(persons.concat({ name: newName, phone: newPhone }));
+		}
+		console.log(persons);
 	};
 
 	return (
@@ -21,11 +23,19 @@ const App = () => {
 			<h2>Phonebook</h2>
 			<form onSubmit={addNewName}>
 				<div>
-					name:{" "}
+					name:
 					<input
 						placeholder="Enter a name"
 						value={newName}
 						onChange={(e) => setNewName(e.target.value)}
+					/>
+				</div>
+
+				<div>
+					number: <input
+						placeholder="Enter a phone number"
+						value={newPhone}
+						onChange={(e) => setNewPhone(e.target.value)}
 					/>
 				</div>
 				<div>
@@ -34,7 +44,7 @@ const App = () => {
 			</form>
 			<h2>Numbers</h2>
 			{persons.map((person) => {
-				return <p key={person.name}>{person.name}</p>;
+				return <p key={person.name}>{person.name}: {person.phone}</p>;
 			})}
 		</div>
 	);
