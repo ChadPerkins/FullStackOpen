c72d1211a8210770b9125160360ab17d5814e55b
ChadPerkins <web3.engineering@gmail.com>
2023-02-20 21:13:50 -0500

    Completed exercise 2.9

--- part2/exercises2.6-2.10/src/App.js
+++ part2/exercises2.6-2.10/src/App.js
@@ -2,18 +2,27 @@ import { useState } from "react";
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", phone: "123-456-7890" },
+		{ name: "Arto Hellas", phone: "040-123456", id: 1 },
+		{ name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
+		{ name: "Dan Abramov", phone: "12-43-234345", id: 3 },
+		{ name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
 	]);
 	const [newName, setNewName] = useState("");
-  const [newPhone, setNewPhone] = useState("");
+	const [newPhone, setNewPhone] = useState("");
+	const [newSearch, setNewSearch] = useState("");
 
 	const addNewName = (event) => {
 		event.preventDefault();
-
 		if (persons.some((e) => e.name === newName)) {
 			alert(`${newName} is already added to the phonebook`);
 		} else {
-			setPersons(persons.concat({ name: newName, phone: newPhone }));
+			setPersons(
+				persons.concat({
+					name: newName,
+					phone: newPhone,
+					id: persons[persons.length - 1].id + 1,
+				})
+			);
 		}
 		console.log(persons);
 	};
@@ -21,6 +30,15 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<p>
+				filter shown with{" "}
+				<input
+					placeholder="Search"
+					value={newSearch}
+					onChange={(e) => setNewSearch(e.target.value)}
+				/>
+			</p>
+			<h2>add a new</h2>
 			<form onSubmit={addNewName}>
 				<div>
 					name:
@@ -32,7 +50,8 @@ const App = () => {
 				</div>
 
 				<div>
-					number: <input
+					number:{" "}
+					<input
 						placeholder="Enter a phone number"
 						value={newPhone}
 						onChange={(e) => setNewPhone(e.target.value)}
@@ -43,9 +62,25 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => {
-				return <p key={person.name}>{person.name}: {person.phone}</p>;
-			})}
+			{persons
+				.filter((p) => {
+					if (newSearch === "") {
+						return p;
+					} else if (
+						p.name
+							.toLocaleLowerCase()
+							.includes(newSearch.toLocaleLowerCase())
+					) {
+						return p;
+					}
+				})
+				.map((p) => {
+					return (
+						<p key={p.id}>
+							{p.name}: {p.phone}
+						</p>
+					);
+				})}
 		</div>
 	);
 };
