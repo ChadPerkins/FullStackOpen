9268a7154d0f5ae2d8e3d8417e502856c87ff21a
ChadPerkins <web3.engineering@gmail.com>
2023-02-23 16:10:00 -0500

    Completed exercise 3.12

---
+++ part3/exercise3.12/README.md
@@ -0,0 +1,1 @@
+Project is deployed to: https://frosty-night-4875.fly.dev/
---
+++ part3/exercise3.12/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+	console.log("Give password as an argument");
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const personName = process.argv[3];
+const personNumber = process.argv[4];
+
+const url = `mongodb+srv://inflamedspirit:${password}@cluster0.jmrpf.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+	Person.find({}).then((res) => {
+		res.forEach((elem) => {
+			console.log(elem);
+		});
+		mongoose.connection.close();
+	});
+}
+
+if (process.argv.length > 3) {
+	const person = new Person({
+		name: personName,
+		number: personNumber,
+	});
+	person.save().then((res) => {
+		console.log("Person added to phonebook.");
+		mongoose.connection.close();
+	});
+}
