e416160fcf7c9b9d773253803e030867088d5039
ChadPerkins <web3.engineering@gmail.com>
2023-02-27 08:38:07 -0500

    Completed exercise 4.3

---
+++ part4/exercises4.3-4.7/app.js
@@ -0,0 +1,16 @@
+const config = require("./utils/config");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blog");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+app.use(blogRouter);
+
+module.exports = app;
---
+++ part4/exercises4.3-4.7/utils/list_helper.js
@@ -0,0 +1,9 @@
+const dummy = ([]) => {
+    return 1;
+  }
+    
+  module.exports = {
+    dummy,
+  }
+
+  
